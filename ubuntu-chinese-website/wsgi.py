265449d953cca1752763b16e1737a9b669370673
Graham Bancroft <graham.bancroft@canonical.com>
2014-06-11 09:06:49 +0100

    Bug fixes

--- ubuntu-chinese-website/wsgi.py
+++ ubuntu-chinese-website/wsgi.py
@@ -6,9 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "canonical.settings")
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ubuntu-chinese-website.settings")
 
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
