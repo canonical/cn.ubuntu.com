fed9adb97be6306739d331dc0cf838c61438ac53
Robin Winslow <robin.winslow@canonical.com>
2014-06-18 15:20:47 +0100

    Make static files work in production - with Cling and dj-static

--- ubuntu-chinese-website/wsgi.py
+++ ubuntu-chinese-website/wsgi.py
@@ -6,14 +6,22 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
+
+# standard
 import os
 import sys
+
+# third party
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(BASE_DIR)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ubuntu-chinese-website.settings")
-
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "ubuntu-chinese-website.settings"
+)
 
 application = get_wsgi_application()
+application = Cling(get_wsgi_application())
