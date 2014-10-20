af78ed6f973cefd5840fe4c878cc185fbf0d4e66
Robin Winslow <robin.winslow@canonical.com>
2014-10-20 22:08:01 +0100

    New modules (versioned-static, static-root-finder); separate out dev settings and requirements

--- webapp/settings.py
+++ webapp/settings.py
@@ -8,17 +8,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SECRET_KEY = 'o@kjnphb9#+3fl80i#$v$+0la3u^atow)b33h*bafbcwir0w04'
 
-DEBUG = True
-TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS = []
+INSTALLED_APPS = ['django_versioned_static_url']
 MIDDLEWARE_CLASSES = []
 ROOT_URLCONF = 'webapp.urls'
 WSGI_APPLICATION = 'webapp.wsgi.application'
@@ -28,8 +25,8 @@ USE_I18N = False
 USE_L10N = False
 USE_TZ = False
 
-# Customisations
-# ===========
 TEMPLATE_DIRS = ["templates"]
-STATICFILES_DIRS = ["static"]
+
+STATICFILES_FINDERS = ['django_static_root_finder.finders.StaticRootFinder']
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
