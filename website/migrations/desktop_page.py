a9f36d0041eb3a9bc5dcde804a1e95d0cfc57dcf
Robin Winslow <robin@robinwinslow.co.uk>
2015-12-06 09:59:17 +0000

    New migration for desktop page

---
+++ website/migrations/desktop_page.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# Modules
+from django.db import migrations
+
+# Local
+from website.db_helpers import import_page_from_json_file
+
+
+def import_page(apps, schema_editor):
+    """
+    Import a new page from a JSON data in the "page-data" directory
+    """
+
+    import_page_from_json_file('website/page-data/desktop.json')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('website', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(import_page)
+    ]
