bdfe5c71892767f71e91166d3bc9da0b9fdfc5f7
Robin Winslow <robin@robinwinslow.co.uk>
2019-01-14 16:36:54 +0000

    Remove extra settings and apps for Django admin
    We no longer use Django admin or the database or Markdown.
    
    This removes all the unnecessary settings, so we can remove the modules.

--- webapp/settings.py
+++ webapp/settings.py
@@ -18,37 +18,16 @@ APPEND_SLASH = True
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django_openid_auth',
     'whitenoise.runserver_nostatic',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_markdown',
-    'markdown_deux',
-    'reversion',
-    'reversion_compare',
     'canonicalwebteam',
 ]
 
-MIDDLEWARE_CLASSES = [
-    'django.middleware.locale.LocaleMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-]
+MIDDLEWARE_CLASSES = []
 
 ROOT_URLCONF = 'webapp.urls'
 
 STATICFILES_FINDERS = [
     'django_static_root_finder.finders.StaticRootFinder',
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
 ]
 STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
@@ -60,13 +39,6 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request'
             ],
         },
@@ -82,36 +54,3 @@ MARKDOWN_DEUX_STYLES = {
 }
 WSGI_APPLICATION = 'webapp.wsgi.application'
 
-# Django openID auth
-# ===
-
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
-
-AUTHENTICATION_BACKENDS = (
-    'django_openid_auth.auth.OpenIDBackend',
-    'django.contrib.auth.backends.ModelBackend',
-)
-
-LOGIN_URL = '/openid/login/'
-LOGIN_REDIRECT_URL = '/'
-
-SUPERUSER_GROUP = 'canonical-webmonkeys'
-GROUP_PERMISSIONS = [
-    'change_element',
-    'change_page',
-    'add_revision',
-    'change_revision',
-    'delete_revision',
-    'add_version',
-    'change_version',
-    'delete_version'
-]
-
-OPENID_CREATE_USERS = True
-OPENID_SSO_SERVER_URL = 'https://login.launchpad.net/'
-OPENID_LAUNCHPAD_TEAMS_REQUIRED = [
-    'canonical-content-people',
-    'canonical-webmonkeys'
-]
-OPENID_USE_AS_ADMIN_LOGIN = True
-OPENID_LAUNCHPAD_TEAMS_MAPPING_AUTO = True
