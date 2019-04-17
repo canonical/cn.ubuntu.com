aafacc763eda89007269bb738e82977e03e3fad0
Thomas Bille <thomas.bille@canonical.com>
2019-04-17 14:29:47 +0100

    Use re_path for regex urls

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import re_path, path, include
 from canonicalwebteam.django_views import TemplateFinder
 from canonicalwebteam.yaml_responses.django_helpers import (
     create_redirect_views,
@@ -7,6 +7,6 @@ from canonicalwebteam.yaml_responses.django_helpers import (
 urlpatterns = create_redirect_views()
 urlpatterns += [
     path(r"blog", include("canonicalwebteam.blog.django.urls")),
-    path(r"<template>", TemplateFinder.as_view()),
+    re_path(r"^(?P<template>.*)$", TemplateFinder.as_view()),
     path(r"", TemplateFinder.as_view()),
 ]
