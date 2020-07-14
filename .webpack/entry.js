1c42a72153cb2bd4273ca8e4425573041ed971c6
Steve Rydz <steve.rydz@canonical.com>
2020-07-14 15:50:33 +0100

    Add newsletter form to blog pages

--- .webpack/entry.js
+++ .webpack/entry.js
@@ -1,4 +1,5 @@
 module.exports = {
   ['global-nav']: './static/js/global-nav.js',
   ['latest-news']: './static/js/latest-news.js',
+  ['forms']: ['./static/js/serialize.js', './static/js/form-validation.js'],
 };
