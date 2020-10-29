b067c2e44f2204b60e3f40a26788cbf7c686e26c
Carlos Wu <carlos.fei@canonical.com>
2020-10-29 12:25:51 +0000

    Setup engage pages and takeovers to discourse

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,15 @@
-import yaml
-import talisker
+import os
 
-from canonicalwebteam.blog import build_blueprint, BlogViews, BlogAPI
+import flask
+import talisker
+import yaml
+from canonicalwebteam import image_template
+from canonicalwebteam.blog import BlogAPI, BlogViews, build_blueprint
+from canonicalwebteam.discourse import DiscourseAPI, EngagePages, EngageParser
 from canonicalwebteam.flask_base.app import FlaskBase
 from canonicalwebteam.templatefinder import TemplateFinder
-from canonicalwebteam import image_template
+
+from webapp.views import build_engage_index, engage_thank_you
 
 app = FlaskBase(
     __name__,
@@ -14,6 +19,83 @@ app = FlaskBase(
     template_404="404.html",
     template_500="500.html",
 )
+
+
+# Engage pages and takeovers from Discourse
+# This section needs to provide takeover data for /
+authenticated_session = talisker.requests.get_session()
+
+authenticated_discourse_api = DiscourseAPI(
+    base_url="https://discourse.ubuntu.com/",
+    session=authenticated_session,
+    api_key=os.getenv("DISCOURSE_API_KEY"),
+    api_username=os.getenv("DISCOURSE_API_USERNAME"),
+)
+
+engage_path = "/engage"
+engage_pages = EngagePages(
+    parser=EngageParser(
+        api=authenticated_discourse_api,
+        index_topic_id=19117,
+        url_prefix=engage_path,
+    ),
+    document_template="/engage/base_engage.html",
+    url_prefix=engage_path,
+    blueprint_name="engage-pages",
+)
+
+app.add_url_rule(engage_path, view_func=build_engage_index(engage_pages))
+
+
+def build_takeovers(engage_pages):
+    def index_page():
+        engage_pages.parser.parse()
+
+        # Show only active
+        active_takeovers = [
+            takeover
+            for takeover in engage_pages.parser.takeovers
+            if takeover["active"] == "true"
+        ]
+        return flask.render_template("index.html", takeovers=active_takeovers)
+
+    return index_page
+
+
+def build_takeovers_index(engage_pages):
+    def takeover_index():
+        engage_pages.parser.parse()
+        sorted_takeovers = sorted(
+            engage_pages.parser.takeovers,
+            key=lambda takeover: takeover["publish_date"],
+            reverse=True,
+        )
+        active_takeovers = [
+            takeover
+            for takeover in engage_pages.parser.takeovers
+            if takeover["active"] == "true"
+        ]
+        active_count = len(active_takeovers)
+        hidden_count = len(sorted_takeovers) - active_count
+        return flask.render_template(
+            "takeovers/index.html",
+            active_count=active_count,
+            hidden_count=hidden_count,
+            takeovers=sorted_takeovers,
+        )
+
+    return takeover_index
+
+
+app.add_url_rule("/", view_func=build_takeovers(engage_pages))
+app.add_url_rule("/takeovers", view_func=build_takeovers_index(engage_pages))
+engage_pages.init_app(app)
+
+app.add_url_rule(
+    "/engage/<page>/thank-you",
+    view_func=engage_thank_you(engage_pages),
+)
+
 
 template_finder_view = TemplateFinder.as_view("template_finder")
 session = talisker.requests.get_session()
---
+++ webapp/views.py
@@ -0,0 +1,73 @@
+import flask
+import math
+
+
+def build_engage_index(engage_docs):
+    def engage_index():
+        page = flask.request.args.get("page", default=1, type=int)
+        topic = flask.request.args.get("topic", default=None, type=str)
+        sort = flask.request.args.get("sort", default=None, type=str)
+        posts_per_page = 15
+        engage_docs.parser.parse()
+        metadata = engage_docs.parser.metadata
+
+        total_pages = math.ceil(len(metadata) / posts_per_page)
+
+        return flask.render_template(
+            "engage/index.html",
+            forum_url=engage_docs.parser.api.base_url,
+            metadata=metadata,
+            page=page,
+            topic=topic,
+            sort=sort,
+            posts_per_page=posts_per_page,
+            total_pages=total_pages,
+        )
+
+    return engage_index
+
+
+def engage_thank_you(engage_pages):
+    """
+    Renders an engage pages thank-you page
+    i.e. whitepapers, pdfs
+
+    If there is no current topic it can't render the page
+    e.g. accessing directly
+
+    @parameters: language (optional) and page path name
+    e.g. /cloud-init-whitepaper
+    @returns: a function that renders a template
+    """
+
+    def render_template(page):
+        engage_pages.parser.parse()
+        page_url = f"/engage/{page}"
+        index_topic_data = next(
+            (item for item in engage_pages.parser.metadata if item["path"] == page_url),
+            None,
+        )
+
+        if index_topic_data:
+            topic_id = engage_pages.parser.url_map[page_url]
+            engage_page_data = engage_pages.parser.get_topic(topic_id)
+            request_url = flask.request.referrer
+            resource_name = index_topic_data["type"]
+            resource_url = engage_page_data["metadata"]["resource_url"]
+            related = [
+                item
+                for item in engage_page_data["related"]
+            ]
+            template = "engage/thank-you.html"
+
+            return flask.render_template(
+                template,
+                request_url=request_url,
+                resource_name=resource_name,
+                resource_url=resource_url,
+                related=related,
+            )
+        else:
+            return flask.abort(404)
+
+    return render_template
