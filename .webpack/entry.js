12c2bcab8e4ee40ea3abfe8452cb2f072e2fd7c7
zprood <liam.zheng@canonical.com>
2022-05-19 18:00:51 +0800

    Update xilinx page

--- .webpack/entry.js
+++ .webpack/entry.js
@@ -2,6 +2,7 @@ module.exports = {
   ['global-nav']: './static/js/src/global-nav.js',
   ['cookie-policy']: './static/js/src/cookie-policy.js',
   ['latest-news']: './static/js/src/latest-news.js',
+  ['tabbed-content.js']: './static/js/src/tabbed-content.js',
   ['forms']: [
     './static/js/third-party/serialize.js',
     './static/js/src/form-validation.js',
