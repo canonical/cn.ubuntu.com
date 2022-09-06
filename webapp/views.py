86c37b56b9890077168c40deebfdbdb8ec4a011d
Carlos Wu <carlos.fei@canonical.com>
2022-09-06 11:23:25 +0200

    Updates to conform to new engage pages (#637)

--- webapp/views.py
+++ webapp/views.py
@@ -8,14 +8,13 @@ def build_engage_index(engage_docs):
         topic = flask.request.args.get("topic", default=None, type=str)
         sort = flask.request.args.get("sort", default=None, type=str)
         posts_per_page = 15
-        engage_docs.parser.parse()
-        metadata = engage_docs.parser.metadata
+        metadata = engage_docs.get_index()
 
         total_pages = math.ceil(len(metadata) / posts_per_page)
 
         return flask.render_template(
             "engage/index.html",
-            forum_url=engage_docs.parser.api.base_url,
+            forum_url=engage_docs.api.base_url,
             metadata=metadata,
             page=page,
             topic=topic,
@@ -27,6 +26,33 @@ def build_engage_index(engage_docs):
     return engage_index
 
 
+def build_engage_page(engage_pages):
+    def engage_page(page):
+        path = f"/engage/{page}"
+        metadata = engage_pages.get_engage_page(path)
+        if not metadata:
+            flask.abort(404)
+        else:
+            return flask.render_template(
+                "engage/base_engage.html",
+                forum_url=engage_pages.api.base_url,
+                metadata=metadata,
+            )
+
+    return engage_page
+
+
+def match_tags(tags_1, tags_2):
+    for tag_1 in tags_1:
+        for tag_2 in tags_2:
+            if tag_1.strip().lower() == tag_2.strip().lower():
+                return True
+            else:
+                continue
+
+    return False
+
+
 def engage_thank_you(engage_pages):
     """
     Renders an engage pages thank-you page
@@ -34,42 +60,37 @@ def engage_thank_you(engage_pages):
 
     If there is no current topic it can't render the page
     e.g. accessing directly
-
-    @parameters: language (optional) and page path name
-    e.g. /cloud-init-whitepaper
-    @returns: a function that renders a template
     """
 
     def render_template(page):
-        engage_pages.parser.parse()
-        page_url = f"/engage/{page}"
-        index_topic_data = next(
-            (
-                item
-                for item in engage_pages.parser.metadata
-                if item["path"] == page_url
-            ),
-            None,
+
+        path = f"/engage/{page}"
+        metadata = engage_pages.get_engage_page(path)
+        all_engage_pages = engage_pages.get_index()
+        if not metadata:
+            flask.abort(404)
+
+        total_num_related = 3
+        related = []
+        for item in all_engage_pages:
+            # Match language and match tags
+            if match_tags(
+                item["tags"].split(","), metadata["tags"].split(",")
+            ):
+                related.append(item)
+            if len(related) < total_num_related:
+                # we can only fit 3 related posts, no need to finish the loop
+                break
+
+        template = "engage/thank-you.html"
+
+        return flask.render_template(
+            template,
+            request_url=flask.request.referrer,
+            metadata=metadata,
+            resource_name=metadata["type"],
+            resource_url=metadata["resource_url"],
+            related=related,
         )
-
-        if index_topic_data:
-            topic_id = engage_pages.parser.url_map[page_url]
-            engage_page_data = engage_pages.parser.get_topic(topic_id)
-            request_url = flask.request.referrer
-            resource_name = index_topic_data["type"]
-            resource_url = engage_page_data["metadata"]["resource_url"]
-            related = [item for item in engage_page_data["related"]]
-            template = "engage/thank-you.html"
-
-            return flask.render_template(
-                template,
-                request_url=request_url,
-                resource_name=resource_name,
-                resource_url=resource_url,
-                engage_page_data=engage_page_data["metadata"],
-                related=related,
-            )
-        else:
-            return flask.abort(404)
 
     return render_template
