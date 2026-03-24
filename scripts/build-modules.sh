3d4a5d1e74fe8686e401b12d038950539b775752
Olwe Samuel <samuel.olwe@canonical.com>
2026-03-24 12:09:48 +0300

    Add build script for vanilla framework modules

---
+++ scripts/build-modules.sh
@@ -0,0 +1,4 @@
+mkdir -p static/js/modules/vanilla-framework/js
+
+cp -r node_modules/vanilla-framework/templates/_macros/ static/js/modules/vanilla-framework
+cp -r node_modules/vanilla-framework/templates/static/js/tabs.js static/js/modules/vanilla-framework/js/.
