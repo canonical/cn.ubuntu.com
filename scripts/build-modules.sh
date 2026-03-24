067658ea582d468bd5e72f59b42c1f4e1348d687
Samuel Olwe <samuel.olwe@canonical.com>
2026-03-24 12:16:03 +0300

    feat: update js build

--- scripts/build-modules.sh
+++ scripts/build-modules.sh
@@ -1,4 +1,3 @@
 mkdir -p static/js/modules/vanilla-framework/js
 
 cp -r node_modules/vanilla-framework/templates/_macros/ static/js/modules/vanilla-framework
-cp -r node_modules/vanilla-framework/templates/static/js/tabs.js static/js/modules/vanilla-framework/js/.
